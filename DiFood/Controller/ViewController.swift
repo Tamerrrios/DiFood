fbec5a47f72223742d64f4d624ace9cef30dc805
Tamer <jtamer98@gmail.com>
2022-05-23 01:21:04 +0500

    Initial Commit

---
+++ DiFood/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiFood
+//
+//  Created by Temur Juraev on 23.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
