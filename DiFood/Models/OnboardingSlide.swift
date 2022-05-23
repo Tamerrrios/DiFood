d5d271232606784d7b6cfa83d5075d30665a2211
Tamer <tamerrrios@gmail.com>
2022-05-23 22:12:27 +0500

    Finish working with Onboarding screen

--- DiFood/Models/OnboardingSlide.swift
+++ DiFood/Models/OnboardingSlide.swift
@@ -13,5 +13,16 @@ struct OnboardingSlide {
     let title: String
     let description: String
     let imgae: UIImage
+    
+    
+    static func getSlide() -> [OnboardingSlide] {
+        [
+        OnboardingSlide(title: "Ваш мобильный официант", description: "Оформите заказ за пару кликов!", imgae: UIImage(imageLiteralResourceName: "slide1")),
+        OnboardingSlide(title: "Отсканируете ваш стол с помощью QR кода", description: "Выберите блюдо и оформите Ваш заказ без участия официанта", imgae: UIImage(imageLiteralResourceName: "slide2")),
+        OnboardingSlide(title: "Оплатите с помощью Вашей карты не дожидаясь счета!", description: "Получайте кэшбеки с каждого заказа", imgae: UIImage(imageLiteralResourceName: "slide3"))
+        ]
+    }
+
 }
+
 
--- DiFood/Views/Onboarding/OnboardingViewController.swift
+++ DiFood/Views/Onboarding/OnboardingViewController.swift
@@ -14,24 +14,45 @@ class OnboardingViewController: UIViewController {
     @IBOutlet var nextButton: UIButton!
     @IBOutlet var pageControl: UIPageControl!
     
-    var slides: [OnboardingSlide] = []
+    var slides = OnboardingSlide.getSlide()
+    
+    var currentPage = 0 {
+        didSet {
+            pageControl.currentPage = currentPage
+            if currentPage == slides.count - 1 {
+                nextButton.setTitle("Начать", for: .normal)
+            } else {
+                nextButton.setTitle("Дальше", for: .normal)
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pageControl.numberOfPages = slides.count
         
-       
         
-
     }
     
 
 
     @IBAction func nextButtonPressed(_ sender: UIButton) {
+        
+        if currentPage == slides.count - 1 {
+            print("Go to next page")
+        } else {
+            currentPage += 1
+            let indexPath = IndexPath(item: currentPage, section: 0)
+            collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
+           
+        }
+        
+        
     }
     
 }
 
-extension OnboardingViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+extension OnboardingViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         slides.count
     }
@@ -42,6 +63,16 @@ extension OnboardingViewController: UICollectionViewDataSource, UICollectionView
         cell.setup(slides[indexPath.row])
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let width = scrollView.frame.width
+        currentPage = Int(scrollView.contentOffset.x / width)
+        
     }
     
     
