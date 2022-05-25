24cff48d173799587be41c7b28d301a396cbda6d
Tamer <tamerrrios@gmail.com>
2022-05-25 22:45:43 +0500

    SearchBar setup finished SearchBar settings with filtering finished

--- DiFood/Controller/HomeViewController.swift
+++ DiFood/Controller/HomeViewController.swift
@@ -7,83 +7,52 @@
 
 import UIKit
 
-class HomeViewController: UITableViewController {
+class HomeViewController: UITableViewController, UISearchBarDelegate  {
+    
+    @IBOutlet var searchBar: UISearchBar!
     
     let cafeList = Cafe.getList()
-
+    var filteredList: [Cafe]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        searchBar.delegate = self
         tableView.rowHeight = 80
-
-       
+        filteredList = cafeList
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        cafeList.count
+        filteredList.count
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cafe", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        let cafe = cafeList[indexPath.row]
+        let cafe = filteredList[indexPath.row]
         
         content.text = cafe.name
         content.image = cafe.image
         content.imageProperties.cornerRadius = tableView.rowHeight / 2
         cell.contentConfiguration = content
-
-
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredList = []
+        if searchText == "" {
+            filteredList = cafeList
+        } else  {
+            for cafe in cafeList {
+                if cafe.name.lowercased().contains(searchText.lowercased()) {
+                    filteredList.append(cafe)
+                }
+            }
+        }
+        self.tableView.reloadData()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
